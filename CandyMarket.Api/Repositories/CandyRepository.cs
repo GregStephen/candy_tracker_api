1ebec423ac105e07ee3310dcdb0916941275e994
Greg Stephen <GregAStephen@gmail.com>
2019-11-02 11:39:05 -0500

    added some repo and updated the sql package

--- CandyMarket.Api/Repositories/CandyRepository.cs
+++ CandyMarket.Api/Repositories/CandyRepository.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using Dapper;
 using CandyMarket.Api.DataModels;
 using CandyMarket.Api.Dtos;
 
@@ -7,19 +9,47 @@ namespace CandyMarket.Api.Repositories
 {
     public class CandyRepository : ICandyRepository
     {
+        string _connectionString = "Server=localhost;Database=CandyMarket;Trusted_Connection=True;";
         public IEnumerable<Candy> GetAllCandy()
         {
-            throw new NotImplementedException();
+            using (var db = new SqlConnection(_connectionString))
+            {
+                var candies = db.Query<Candy>
+                   (
+                   @"SELECT * 
+                      FROM Candy"
+                   );
+                return candies;
+            }
         }
 
         public bool AddCandy(AddCandyDto newCandy)
         {
-            throw new NotImplementedException();
+            using (var db = new SqlConnection(_connectionString))
+            {
+                var sql = @"
+                    INSERT INTO [Candy]
+                        ([Name]
+                        ,[Type]
+                        ,[Price])
+	                OUTPUT insterted.*
+                    VALUES
+                        (@name
+                        ,@type
+                        ,@price)";
+                return db.Execute(sql, newCandy) == 1;
+            }
         }
 
         public bool EatCandy(Guid candyIdToDelete)
         {
-            throw new NotImplementedException();
+            using (var db = new SqlConnection(_connectionString))
+            {
+                var sql = @"DELETE
+                            FROM Candy
+                            WHERE [Id] = @id";
+                return db.Execute(sql, new { id = candyIdToDelete }) == 1;
+            }
         }
     }
 }
