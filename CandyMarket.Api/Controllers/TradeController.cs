38f4f1b9ca16d7cb7787309dc581b42113dc3efd
Greg Stephen <GregAStephen@gmail.com>
2019-11-10 14:31:06 -0600

    added a trade repo to start pulling in trades

---
+++ CandyMarket.Api/Controllers/TradeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CandyMarket.Api.DataModels;
+using CandyMarket.Api.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CandyMarket.Api.Controllers
+{
+    [ApiController]
+    [Route("trade")]
+
+    public class TradeController : ControllerBase
+    {
+        private readonly ILogger<TradeController> _logger;
+        private readonly ITradeRepository _repo;
+
+        public TradeController(ILogger<TradeController> logger, ITradeRepository repo)
+        {
+            _logger = logger;
+            _repo = repo;
+        }
+        [HttpGet]
+        public IEnumerable<Trade> GetAll()
+        {
+            return _repo.GetAllTrades();
+        }
+    }
+}
---
+++ CandyMarket.Api/Repositories/ITradeRepository.cs
@@ -0,0 +1,13 @@
+﻿using CandyMarket.Api.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandyMarket.Api.Repositories
+{
+    public interface ITradeRepository
+    {
+        IEnumerable<Trade> GetAllTrades();
+    }
+}
